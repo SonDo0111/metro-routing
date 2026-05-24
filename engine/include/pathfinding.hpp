444026c661fae3c060bef65ff38b4822870210f1
SonDo0111 <thea73092@gmail.com>
2026-05-25 01:54:24 +0700

    Complete dijkstra function, prepare for update to AStar function

---
+++ engine/include/pathfinding.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <queue>
+#include <string_view>
+#include <cstddef>
+#include <algorithm>
+#include "graph.hpp"
+
+enum class RoutingProfile
+{
+    COMMUTER = 0,        // "Return the fastest itinerary"
+    BALANCED = 300,      // "Return the balanced itinerary, it's equivalent to if transfer(transfer + wait) take less than 5 mins then it's acceptable" (Default)
+    TOURIST = 900,       // "Return the slower itinerary but less transfer"
+    NO_TRANSFERS = 99999 // "Retunrn the itinerary without transfer unless it is literally the only way."
+};
+
+constexpr GTFSData::weight_t INF = std::numeric_limits<GTFSData::weight_t>::max() / 2;
+
+struct QueueElement
+{
+    GTFSData::weight_t time_taken;
+    GTFSData::node_t nodeID;
+
+    bool operator>(const QueueElement &other) const
+    {
+        return this->time_taken > other.time_taken;
+    }
+};
+
+struct PathResult
+{
+    GTFSData::weight_t total_time;
+    std::vector<GTFSData::node_t> path_nodes;
+};
+
+std::size_t transfer_count(const PathResult &result);
+
+PathResult dijkstra(const GTFSData::graph_t &graph,
+                    GTFSData::node_t start_node, GTFSData::node_t goal_node,
+                    GTFSData::weight_t transfer_penalty = static_cast<int>(RoutingProfile::BALANCED));
+
+void print_itinerary(const PathResult &result);
